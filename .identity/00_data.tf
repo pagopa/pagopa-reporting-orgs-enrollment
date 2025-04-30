edafbbd857dfa1ed4fab8d7c5c31baa6c68a3ea0
svariant <samuele.varianti@nttdata.com>
2025-04-30 12:35:50 +0200

    chore: [PIDM-491] Fix name workload

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -62,7 +62,7 @@ data "azurerm_key_vault_secret" "key_vault_integration_subkey" {
 }
 
 data "azurerm_user_assigned_identity" "workload_identity_clientid" {
-  name                = "receipts-workload-identity"
+  name                = "gps-workload-identity"
   resource_group_name = "pagopa-${var.env_short}-${local.location_short}-${var.env}-aks-rg"
 }
 
